9b16d39b6f28e3a228d2c70d43e47e1535e73f96
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-29 14:45:10 -0500

    added POST route for /urls/:id to update a resource

--- express_server.js
+++ express_server.js
@@ -57,11 +57,17 @@ app.get("/urls/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+// Post route to update a URL
+app.get("/urls/:id", (req, res) => {
+  res.redirect("/urls/:id")
+})
+
 //Post to delete the URL 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect("/urls/");
 })
+
 
 
 // app.get("/", (req, res) => {
