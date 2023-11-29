ee4da80313a7471728e993c1e64fd836cca1ac87
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-29 02:12:33 -0500

    added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-  return Math.random().toString(36).substring(2,8)
+  return Math.random().toString(36).substring(2,8); 
 }
 
 const urlDatabase = {
