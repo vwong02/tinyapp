968875e9a41ade0b0bc0c73336f6e5f7905eabe2
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-29 03:25:44 -0500

    added comments to keep track of each route. Fixed upt he redirect because I was getting errors. Now it should redirect to the new short URL with the new id generated and post it to /urls

--- express_server.js
+++ express_server.js
@@ -4,8 +4,10 @@ const PORT = 8080; // default port 8080
 
 app.use(express.urlencoded({ extended: true }));
 
+// Tells Express app to use EJS as it's templating engine
 app.set("view engine", "ejs");
 
+// Function to generate a random alphanumeric string that's 6 characters long
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
@@ -15,47 +17,60 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${ PORT }!`);
 });
 
+// Adds route for the JSON responst with the urlDatabase data
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${ a }`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${ a }`);
-});
-
+// Route to show all the shortened URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  // Renders the urls_index.ejs and shows the list of URLs with the templateVars
   res.render("urls_index", templateVars);
 });
 
+// Route to where users can add a new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Route to display the specific URL with a specific id and render the urls_show.ejs
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+// Generates a random id, posts the new id and longURL on /urls and redirects to the specific urls page with the new id
 app.post("/urls", (req, res) => {
   let id = generateRandomString()
   urlDatabase[id] = req.body.longURL
   res.redirect(`/urls/${ id }`);
 });
+
+// Redirects to the longURL page
+app.get("/urls/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${ a }`);
+// });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${ a }`);
+// });
