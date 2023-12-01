4405fd42342df19c77b9b80d5a8d095ea4188101
Victoria Wong <victoria.ys.wong@gmail.com>
2023-12-01 16:07:44 -0500

    added test cases to test if getUserIDByEmail function is working properly

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserIDByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserIDByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = getUserIDByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.isTrue(expectedUserID === user)
+  });
+
+  it('should return undefined if no user email was found', function() {
+    const user = getUserIDByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.isFalse(expectedUserID === undefined)
+  });
+
+});
